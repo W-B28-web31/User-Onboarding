60517bf8bf7c41528e4a9309e36f5d011471b405
William Bell <wtb2804@gmail.com>
2020-05-21 22:26:13 -0500

    MVP

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+
+import axios from 'axios'
 import * as yup from 'yup'
 
 import Form from './components/Form'
-import Card from './components/Card'
+
+
 
 function App() {
+
+  // Ability to toggle the button when fields are complete 
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+
+  const[ users, setUsers ] = useState([])
+const addNewUser = user => {
+  setUsers([...users, user])
+}
+
+
   return (
     <div className="App">
       <h1>New Here? Sign Up!</h1>
-      <Form />
-      <Card />
+      <Form addNewUser={addNewUser} />
+      <ul>
+        {users.map(user => <li key={user.id}> {user.name} {user.email}</li>)}
+      </ul>
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,146 +1,77 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import * as yup from 'yup'
 
 
-const formSchema = yup.object().shape({
-    name: yup
-            .string()
-            .trim()
-            .required("Name is Required."),
-    email: yup
-            .string()
-            .email("Please Enter a Valid Email Address."),
-            // .required("Please Enter a Valid Email Address."),
-    password: yup
-            .string()
-            // .matches(/^[a-zA-Z0-9!@#$%^&*]+$/)
-            .min(5)
-            .required(),
-    terms: yup.boolean().oneOf([true], "please agree to terms of use"),
-    // role: yup
-    //         .required("Please Select a Role"),
-
-})
+const schema = yup.object().shape({
+    name: yup.string().min(2).max(15).required(),
+    email: yup.string().email(),
+    password: yup.string().min(6).max(12).required(),
+    tos: yup.boolean().required()
+});
 
 
-const Form = _ => {
+
+
+const Form = props => {
     
+    const [ user, setUser ] = useState({
+        name:'',
+        email:'',
+        password: '',
+        tos: false
+    })
 
-    // Ability to toggle the button when fields are complete 
-    const [ buttonDisabled, setButtonDisabled ] = useState(true)
+    const handleChange = ( {target: { name, value, checked}} ) => {
+        value = name === 'tos' ? checked : value;
+        setUser(user => ({
+            ...user,
+            [name]:value
+        }));
+    };
+    const [errors, setErrors] = useState([]);
 
- 
-    // Initial Form state 
-    const [formState, setFormState] = useState({
-        name: "",
-        email: "",
-        password: "",
-        role: "",
-        terms: ""
-    });
-
-
-
-    const [errors, setErrors] = useState({
-        name: "",
-        email: "",
-        password: "",
-        role: "",
-        terms: ""
-    });
-
-
-
-
-    useEffect(() => {
-        formSchema.isValid(formState).then(valid => {
-            setButtonDisabled(!valid)
-        })
-    }, [formState])
-
-
-
-    const [post, setPost] = useState([])
-
-
-
-    const formSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
-        axios
-            .post("https://reqres.in/api/users", formState)
-            .then(response => {
-                console.log(response.data)
-                setPost(response.data)
-                setFormState({
-                    name: "",
-                    email: "",
-                    password: "",
-                    role: "",
-                    terms: ""
-                })
+        // console.log(user)
+        schema.validate(user)
+            .then(values => {
+                axios.post('https://reqres.in/api/users', values)
+                    .then(response => {
+                        props.addNewUser(response.data);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                    });
             })
-            .catch(err => console.log("API Response Error", err.response))
-    }
-
-    const validateChange = e => {
-        // Reach will allow us to "reach" into the schema and test only one part.
-        yup
-            .reach(formSchema, e.target.name)
-            .validate(e.target.value)
-            .then(valid => {
-                setErrors({
-                    ...errors,
-                    [e.target.name]: ""
-                });
+            .catch(error => {
+                setErrors(error.errors)
             })
-            .catch(err => {
-                setErrors({
-                    ...errors,
-                    [e.target.name]: err.errors[0]
-                });
-            });
+        
     };
 
-
-    const inputChange = e => {
-        e.persist();
-        const newFormData = {
-            ...formState, [e.target.name]:
-                e.target.type === "checkbox" ? e.target.checked : e.target.value
-        };
-
-        // validateChange(e);
-        setFormState(newFormData);
-    };
-
-
-
-
-
+    
     return (
         <div>
-            <form onSubmit={formSubmit}>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="name">
                     Name
                 <input
                         id="name"
                         type="text"
                         name="name"
-                        value={formState.name}
-                        onChange={inputChange}
+                        value={user.name}
+                        onChange={handleChange}
                     />
                 </label>
-                <br />
                 <label htmlFor="email">
                     Email
                 <input
                         id="email"
                         type="text"
                         name="email"
-                        value={formState.email}
-                        onChange={inputChange}
-                        required
+                        value={user.email}
+                        onChange={handleChange}
                     />
                 </label>
                 <br />
@@ -151,16 +82,16 @@ const Form = _ => {
                         type="password"
                         name="password"
                         minLength="8"
-                        value={formState.password}
-                        onChange={inputChange}
+                        value={user.password}
+                        onChange={handleChange}
                         required
                     />
                 </label>
-                <br />
-                <label>Role
+                <br /> 
+                {/* <label>Role
                     <select
                         onChange={inputChange}
-                        value={formState.role}
+                        value={props.role}
                         name="role"
                     >
                         <option value=''>- Select an option -</option>
@@ -170,20 +101,29 @@ const Form = _ => {
                         <option value='TL'>Team Lead</option>
                     </select>
                 </label>
-                <br />
+                <br /> */}
                 <label htmlFor="checkbox">
                     Agree to Terms?
                 <input
                         id="checkbox"
                         type="checkbox"
-                        checked={formState.role}
-                        name="checkbox"
-                        onChange={inputChange}
+                        checked={user.tos}
+                        name="tos"
+                        onChange={handleChange}
                     />
                 </label>
                 <br />
-                <button disabled={!buttonDisabled}>Submit</button>
+                <button>Submit</button>
             </form>
+
+            {errors.length > 0 && (
+                <div className="errors">
+                    {errors.map(error => (
+                        <p key={error}>{error}</p>
+                    ))}
+
+                </div>
+            )}
         </div>
     )
 }
